2aa644a6f1513bbfedc8f37689168464cac2b0fe
Luke Malpass <contact@angelsix.com>
2019-11-13 18:14:07 +0000

    Meta heads and page title support, mobile address bar color

--- grid/src/router.js
+++ grid/src/router.js
@@ -6,19 +6,23 @@ import GridDemo from "./views/GridDemo.vue";
 
 Vue.use(Router);
 
-export default new Router({
+// Website title
+var title = "A Chimney Sweep";
+
+var router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
       path: "/",
       name: "home",
-      component: Home
+      component: Home,
     },
     {
       path: "/about",
       name: "about",
-      component: About
+      component: About,
+      meta: { title: 'About' }
     },
     {
       path: "/griddemo",
@@ -29,3 +33,16 @@ export default new Router({
     { path: "*", component: Home }
   ]
 });
+
+// Set title of page on load
+router.beforeEach((to, from, next) => {
+
+  // Add site title to end of page title
+  var pageTitle = to.meta.title ? to.meta.title + ' - ' : '';
+
+  document.title = pageTitle + title;
+
+  next();
+});
+
+export default router; 
