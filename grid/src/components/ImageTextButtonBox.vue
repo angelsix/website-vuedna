e77677c85e501077420e13839666d8b55ca57298
Luke Malpass <contact@angelsix.com>
2019-11-16 20:51:40 +0000

    Button style

--- grid/src/components/ImageTextButtonBox.vue
+++ grid/src/components/ImageTextButtonBox.vue
@@ -1,11 +1,17 @@
 <template>
 	<ShadowBox :fullHeight="fullHeight">
-		<Section pad class="center-content">
-			<a :href="imageUrl"><img class="image" :src="imagePath"/></a>
+		<Section pad class="image-text-button center-content">
+			<div class="items">
+				<a :href="imageUrl" target="_blank"
+					><img class="image" :src="imagePath"
+				/></a>
 
-			<h1>{{ title }}</h1>
-			<slot></slot>
-			<a href="#">{{ buttonText }}</a>
+				<h1>{{ title }}</h1>
+				<div class="content">
+					<slot></slot>
+				</div>
+				<a v-if="imageUrl" href="#" class="read-more">{{ buttonText }}</a>
+			</div>
 		</Section>
 	</ShadowBox>
 </template>
@@ -23,8 +29,33 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-.image {
-	width: 6em;
+<style lang="scss">
+.image-text-button {
+	height: 100%;
+	.container {
+		height: 100%;
+
+		.image {
+			width: 6em;
+		}
+
+		.read-more {
+			padding: 1em 2em;
+			color: white;
+			background: red;
+			border-radius: 0.5em;
+			margin: 0 auto;
+		}
+
+		.items {
+			height: 100%;
+
+			display: flex;
+			flex-direction: column;
+			.content {
+				flex-grow: 1;
+			}
+		}
+	}
 }
 </style>
